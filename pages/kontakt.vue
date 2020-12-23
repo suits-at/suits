2f1c821f8c6d66648ff2a9f3df5bfd426df9811f
SUITS <office@suits.at>
2020-12-23 23:45:58 +0100

    refactor: small design adjustments

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
+  <section class="override-link-color">
     <h1>//{{ title }}</h1>
     <div class="mb-8 max-w-4xl" v-html="$md.render(content)" />
-    <div class="flex flex-wrap justify-around">
-      <div class="w-full sm:w-1/3 text-center py-4">
+    <div class="flex flex-wrap">
+      <div class="w-full sm:w-1/3 text-center my-4">
         <font-awesome-icon :icon="['fal', iconMe]" size="3x" color="#646464" />
         <div v-html="$md.render(textMe)" />
       </div>
