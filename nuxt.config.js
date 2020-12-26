6622f2e160d60b34240e74aa1b7d70cd584bd3b2
SUITS <office@suits.at>
2020-12-26 16:32:17 +0100

    refactor: adjust meta title

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'SUITS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
