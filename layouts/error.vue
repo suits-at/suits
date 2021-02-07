bc52843d29e74b06ca7464b56dc104a5924c8639
SUITS <office@suits.at>
2021-02-07 13:23:36 +0100

    feat: add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="flex flex-col justify-center text-center">
+    <h3>Diese Seite konnte nicht gefunden werden.</h3>
+    <p>
+      Bitte überprüfen Sie die URL oder kehren Sie zurück zur
+      <a href="/">Starteite</a>.
+    </p>
+  </main>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  components: {},
+  layout: 'error',
+});
+</script>
