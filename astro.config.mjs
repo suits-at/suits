f665cb023d22bbf06535b5c1cd1294edd3d11b39
suits-at <office@suits.at>
2026-06-22 23:56:25 +0200

    Disable Astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://www.suits.at',
   trailingSlash: 'always',
+  devToolbar: {
+    enabled: false,
+  },
 });
